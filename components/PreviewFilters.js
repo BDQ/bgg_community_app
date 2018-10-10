8f76b918e20f31641356e8f4df32319c2ce7355c
Brian Quinn <briandquinn@gmail.com>
2018-10-10 12:58:44 +0100

    Make filters scrollable

--- components/PreviewFilters.js
+++ components/PreviewFilters.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import { TagSelect } from 'react-native-tag-select'
 import { Button } from 'react-native-elements'
 import RadioForm from 'react-native-simple-radio-button'
@@ -52,104 +52,106 @@ export default class PreviewFilters extends React.Component {
     )
 
     return (
-      <View style={styles.mainView}>
-        <Text style={styles.formHeader}>Sorting</Text>
-        <View style={{ padding: 10 }}>
-          <RadioForm
-            radio_props={sortingOptions}
-            initial={sortIndex}
-            onPress={value => this.setState({ sortBy: value })}
-          />
-        </View>
-        <Text style={styles.formHeader}>Filters</Text>
-        <View style={{ padding: 10 }}>
-          <TouchableOpacity
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingBottom: 8
-            }}
-            onPress={() => this.toggleTags('priorityTags')}
-          >
-            <Text style={styles.formLabel}>Priority</Text>
-            <Text style={styles.toggleText}>(Toggle All)</Text>
-          </TouchableOpacity>
-
-          <TagSelect
-            labelAttr="name"
-            value={filters.priorities}
-            ref={tag => {
-              this.priorityTags = tag
-            }}
-            data={priorities}
-            theme="info"
-          />
-
-          <TouchableOpacity
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              paddingBottom: 8
-            }}
-            onPress={() => this.toggleTags('hallTags')}
-          >
-            <Text style={styles.formLabel}>Halls</Text>
-            <Text style={styles.toggleText}>(Toggle All)</Text>
-          </TouchableOpacity>
-
-          <TagSelect
-            labelAttr="name"
-            value={filters.halls}
-            ref={tag => {
-              this.hallTags = tag
-            }}
-            data={halls}
-            theme="info"
-          />
-
-          <View
-            style={{
-              marginTop: 30,
-              flex: 1,
-              alignSelf: 'stretch',
-              flexDirection: 'row',
-              justifyContent: 'center'
-            }}
-          >
-            <Button
-              style={{ flex: 1 }}
-              backgroundColor="#03A9F4"
-              title="Apply Filters"
-              blue
-              onPress={() => {
-                setFilters(
-                  {
-                    priorities: this.priorityTags.itemsSelected.map(
-                      priority => priority.id || priority
-                    ),
-                    halls: this.hallTags.itemsSelected.map(
-                      hall => hall.id || hall
-                    )
-                  },
-                  this.state.sortBy
-                )
-
-                pop()
-              }}
-            />
-            <Button
-              style={{ flex: 1 }}
-              title="Reset"
-              onPress={() => {
-                this.setState({ filters: defaultFilters })
-
-                this.priorityTags.setState({ value: [] })
-                this.hallTags.setState({ value: [] })
-              }}
+      <ScrollView>
+        <View style={styles.mainView}>
+          <Text style={styles.formHeader}>Sorting</Text>
+          <View style={{ padding: 10 }}>
+            <RadioForm
+              radio_props={sortingOptions}
+              initial={sortIndex}
+              onPress={value => this.setState({ sortBy: value })}
             />
           </View>
+          <Text style={styles.formHeader}>Filters</Text>
+          <View style={{ padding: 10 }}>
+            <TouchableOpacity
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingBottom: 8
+              }}
+              onPress={() => this.toggleTags('priorityTags')}
+            >
+              <Text style={styles.formLabel}>Priority</Text>
+              <Text style={styles.toggleText}>(Toggle All)</Text>
+            </TouchableOpacity>
+
+            <TagSelect
+              labelAttr="name"
+              value={filters.priorities}
+              ref={tag => {
+                this.priorityTags = tag
+              }}
+              data={priorities}
+              theme="info"
+            />
+
+            <TouchableOpacity
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingBottom: 8
+              }}
+              onPress={() => this.toggleTags('hallTags')}
+            >
+              <Text style={styles.formLabel}>Halls</Text>
+              <Text style={styles.toggleText}>(Toggle All)</Text>
+            </TouchableOpacity>
+
+            <TagSelect
+              labelAttr="name"
+              value={filters.halls}
+              ref={tag => {
+                this.hallTags = tag
+              }}
+              data={halls}
+              theme="info"
+            />
+
+            <View
+              style={{
+                marginTop: 30,
+                flex: 1,
+                alignSelf: 'stretch',
+                flexDirection: 'row',
+                justifyContent: 'center'
+              }}
+            >
+              <Button
+                style={{ flex: 1 }}
+                backgroundColor="#03A9F4"
+                title="Apply Filters"
+                blue
+                onPress={() => {
+                  setFilters(
+                    {
+                      priorities: this.priorityTags.itemsSelected.map(
+                        priority => priority.id || priority
+                      ),
+                      halls: this.hallTags.itemsSelected.map(
+                        hall => hall.id || hall
+                      )
+                    },
+                    this.state.sortBy
+                  )
+
+                  pop()
+                }}
+              />
+              <Button
+                style={{ flex: 1 }}
+                title="Reset"
+                onPress={() => {
+                  this.setState({ filters: defaultFilters })
+
+                  this.priorityTags.setState({ value: [] })
+                  this.hallTags.setState({ value: [] })
+                }}
+              />
+            </View>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     )
   }
 }
