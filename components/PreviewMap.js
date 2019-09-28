f205394b9727e9db633a1c2997879ed060539760
Brian Quinn <briandquinn@gmail.com>
2019-09-28 12:39:44 +0100

    more wip

--- components/PreviewMap.js
+++ components/PreviewMap.js
@@ -1,173 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Animated, StyleSheet } from 'react-native'
-import {
-  PanGestureHandler,
-  PinchGestureHandler,
-  State
-} from 'react-native-gesture-handler'
-import { Icon } from 'react-native-elements'
+import { StyleSheet, View } from 'react-native'
+import MapView, { Marker, Overlay, UrlTile, LocalTile } from 'react-native-maps'
 
-// import styles from '../shared/styles'
-
-const MIN_SCALE = 0.2
-const MAX_SCALE = 1.6
-
-export class PinchableBox extends React.Component {
-  panRef = React.createRef()
-  pinchRef = React.createRef()
-  constructor(props) {
-    super(props)
-
-    /* Pinching */
-    this._baseScale = new Animated.Value(1)
-    this._pinchScale = new Animated.Value(1)
-    this._scale = Animated.multiply(
-      this._baseScale,
-      this._pinchScale
-    ).interpolate({
-      inputRange: [MIN_SCALE, MAX_SCALE],
-      outputRange: [MIN_SCALE, MAX_SCALE],
-      extrapolate: 'clamp'
-    })
-
-    this._lastScale = 1
-    this._onPinchGestureEvent = Animated.event(
-      [
-        {
-          nativeEvent: {
-            scale: this._pinchScale
-          }
-        }
-      ],
-      { useNativeDriver: false }
-    )
-
-    /* Pan */
-    this._translateX = new Animated.Value(0)
-    this._translateY = new Animated.Value(0)
-    this._lastOffset = { x: 0, y: 0 }
-    this._onPanGestureEvent = Animated.event(
-      [
-        {
-          nativeEvent: {
-            translationX: this._translateX,
-            translationY: this._translateY
-          }
-        }
-      ],
-      { useNativeDriver: true }
-    )
-  }
-
-  _onPinchHandlerStateChange = event => {
-    console.log('pinch event')
-    console.log({
-      scale: this._scale,
-      lastScale: this._lastScale,
-      translateX: this._translateX,
-      translateY: this._translateY
-    })
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      this._lastScale *= event.nativeEvent.scale
-
-      if (this._lastScale < MIN_SCALE) this._lastScale = MIN_SCALE
-      if (this._lastScale > MAX_SCALE) this._lastScale = MAX_SCALE
-
-      this._baseScale.setValue(this._lastScale)
-      this._pinchScale.setValue(1)
-    }
-  }
-
-  _onPanGestureStateChange = event => {
-    console.log('pan event')
-    console.log({
-      scale: this._scale,
-      translateX: this._translateX,
-      translateY: this._translateY
-    })
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      this._lastOffset.x += event.nativeEvent.translationX
-      this._lastOffset.y += event.nativeEvent.translationY
-      this._translateX.setOffset(this._lastOffset.x)
-      this._translateX.setValue(0)
-      this._translateY.setOffset(this._lastOffset.y)
-      this._translateY.setValue(0)
-    }
-  }
-
-  render() {
-    return (
-      <PinchGestureHandler
-        ref={this.pinchRef}
-        simultaneousHandlers={this.rotationRef}
-        onGestureEvent={this._onPinchGestureEvent}
-        onHandlerStateChange={this._onPinchHandlerStateChange}
-      >
-        <Animated.View style={styles.container}>
-          <PanGestureHandler
-            ref={this.panRef}
-            onGestureEvent={this._onPanGestureEvent}
-            onHandlerStateChange={this._onPanGestureStateChange}
-            // minDist={10}
-            maxPointers={1}
-            avgTouches
-          >
-            <Animated.View style={styles.container} collapsable={false}>
-              <Animated.View
-                style={{
-                  position: 'absolute',
-
-                  zIndex: 9,
-                  transform: [
-                    { scale: this._scale },
-                    {
-                      translateX: Animated.add(this._translateX, 123)
-                    },
-                    {
-                      translateY: Animated.add(this._translateY, 123)
-                    }
-                  ]
-                }}
-              >
-                <Icon
-                  name="location-pin"
-                  iconStyle={{
-                    marginRight: 10,
-                    marginTop: 10,
-                    color: 'red'
-                  }}
-                  type="entypo"
-                  onPress={() => console.log('click')}
-                />
-              </Animated.View>
-              <Animated.Image
-                style={[
-                  styles.pinchableImage,
-                  {
-                    transform: [
-                      { perspective: 200 },
-                      { scale: this._scale },
-                      { translateX: this._translateX },
-                      { translateY: this._translateY }
-                    ]
-                  }
-                ]}
-                source={require('../assets/halls/essen/h1&2.jpeg')}
-              />
-            </Animated.View>
-          </PanGestureHandler>
-        </Animated.View>
-      </PinchGestureHandler>
-    )
-  }
-}
+import map from '../assets/halls/essen/h1-2.png'
 
 export default class PreviewMap extends React.PureComponent {
+  state = {
+    x: 1
+  }
   render() {
     // const { location } = this.props.navigation.state.params
 
-    return <PinchableBox />
+    return (
+      <View style={styles.container}>
+        <MapView
+          zoomControlEnabled={true}
+          style={styles.map}
+          initialRegion={{
+            latitude: 51.427790453806146,
+            latitudeDelta: 0.002539088740931561,
+            longitude: 6.994015671123037,
+            longitudeDelta: 0.0025975481686373314
+          }}
+          // camera={{
+          //   center: {
+          //     latitude: 51.42779045380615,
+          //     longitude: 6.994015671123038
+          //   },
+          //   pitch: 0,
+          //   heading: -6,
+          //   altitude: 500,
+          //   zoom: 16
+          // }}
+          // onRegionChange={(a, b, c) => console.log(a, b, c)}
+        >
+          {/* <Marker
+          draggable
+          coordinate={{
+            latitude: 51.4272,
+            longitude: 6.9957
+          }}
+          onDragEnd={e => console.log(e.nativeEvent.coordinate)}
+        /> */}
+          <Overlay
+            style={{ zIndex: 999 }}
+            image={map}
+            bounds={[[51.42869, 6.99275], [51.4267, 6.9957]]}
+          />
+        </MapView>
+      </View>
+    )
   }
 }
 
@@ -183,18 +66,15 @@ PreviewMap.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'black',
-    overflow: 'hidden',
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center'
+    alignItems: 'center'
   },
-  pinchableImage: {
-    // ...StyleSheet.absoluteFillObject,
-    // flex: 1
+  map: {
+    ...StyleSheet.absoluteFillObject
   },
-  wrapper: {
-    flex: 1
+  overlay: {
+    position: 'absolute',
+    bottom: 50,
+    backgroundColor: 'rgba(255, 255, 255, 1)'
   }
 })
