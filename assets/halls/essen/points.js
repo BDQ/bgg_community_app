9bb3d0e713ba3a0655822bd282d30201ce8877b6
Brian Quinn <briandquinn@gmail.com>
2019-10-01 20:10:30 +0100

    Add points data

---
+++ assets/halls/essen/points.js
@@ -0,0 +1,10 @@
+export const points = {
+  '2F111': {
+    latitude: 51.4274422834481,
+    longitude: 6.994187167938094
+  },
+  '2A113': {
+    latitude: 51.42778949021053,
+    longitude: 6.993877638860226
+  }
+}
