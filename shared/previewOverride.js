90ded7cfce6f07d0088cc233bffa2f1436c4390a
Brian Quinn <briandquinn@gmail.com>
2020-01-16 08:14:17 +0000

    add missing override file

---
+++ shared/previewOverride.js
@@ -0,0 +1,172 @@
+export const companiesOverride = [
+  {
+    publisherId: '47',
+    location: '1D121',
+    locationParsed: '1D121'
+  },
+  {
+    publisherId: '267',
+    location: '5D120',
+    locationParsed: '5D120'
+  },
+  {
+    publisherId: '8',
+    location: '3H103',
+    locationParsed: '3H103'
+  },
+  {
+    publisherId: '36038',
+    location: '5K118',
+    locationParsed: '5K118'
+  },
+  {
+    publisherId: '41409',
+    location: '5K119',
+    locationParsed: '5K119'
+  },
+  {
+    publisherId: '1210',
+    location: '2A110',
+    locationParsed: '2A110'
+  },
+  { publisherId: '43630', notFound: true },
+  {
+    publisherId: '11368',
+    location: '5H119',
+    locationParsed: '5H119'
+  },
+  {
+    publisherId: '265',
+    location: '3G102',
+    locationParsed: '3G102'
+  },
+  {
+    publisherId: '26185',
+    location: '5C132',
+    locationParsed: '5C132'
+  },
+  {
+    publisherId: '17441',
+    location: '1F132',
+    locationParsed: '1F132'
+  },
+  {
+    publisherId: '384',
+    location: '3F102',
+    locationParsed: '3F102'
+  },
+  {
+    publisherId: '18021',
+    location: '3F100',
+    locationParsed: '3F100'
+  },
+  {
+    publisherId: '18021',
+    location: '3F100',
+    locationParsed: '3F100'
+  },
+  {
+    publisherId: '38505',
+    location: '1B139 / 1C131',
+    locationParsed: '1B139'
+  },
+  {
+    publisherId: '30605',
+    location: '1G157',
+    locationParsed: '1G157'
+  },
+  {
+    publisherId: '24945',
+    location: '4E122',
+    locationParsed: '4E122'
+  },
+  {
+    publisherId: '40852',
+    location: '5N121',
+    locationParsed: '5N121'
+  },
+  {
+    publisherId: '40373',
+    location: '1D129',
+    locationParsed: '1D129'
+  },
+
+  {
+    publisherId: '18529',
+    location: '4F121 Surprised Star',
+    locationParsed: '4F121'
+  },
+  {
+    publisherId: '38363',
+    location: '3M101 Pegasus Spiele',
+    locationParsed: '3M101'
+  },
+  {
+    publisherId: '304',
+    location: '3J108',
+    locationParsed: '3J108'
+  },
+  {
+    publisherId: '34188',
+    location: '3O117',
+    locationParsed: '3O117'
+  },
+  {
+    publisherId: '28072',
+    location: '2F138',
+    locationParsed: '2F138'
+  },
+  {
+    publisherId: '4384',
+    location: '3B109 / 3D103',
+    locationParsed: '3B109'
+  },
+  {
+    publisherId: '13288',
+    notFound: true
+  },
+  {
+    publisherId: '31466',
+    location: '5B106',
+    locationParsed: '5B106'
+  },
+  {
+    publisherId: '550',
+    location: '2-C148',
+    locationParsed: '2-C148'
+  },
+  {
+    publisherId: '12106',
+    notFound: true
+  },
+  {
+    publisherId: '34367',
+    notFound: true
+  },
+  {
+    publisherId: '42315',
+    location: '1-G124',
+    locationParsed: '1-G124'
+  },
+
+  {
+    publisherId: '1015',
+    location: '3-D110',
+    locationParsed: '3-D110'
+  },
+  {
+    publisherId: '41441',
+    location: '2-A122',
+    locationParsed: '2-A122'
+  },
+  {
+    publisherId: '11420',
+    location: '2-D138',
+    locationParsed: '2-D138'
+  },
+  {
+    publisherId: '42072',
+    location: '5-M104',
+    locationParsed: '5-M104'
+  }
+]
